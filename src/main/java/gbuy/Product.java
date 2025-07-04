0f1fe9c6633b61cafe2090b16f843d68eebe4905
Andric Quinn Baticos <andric.baticos@gcash.com>
2025-07-04 14:24:04 +0800

    add documentation; add setters for productName, productPrice

--- src/main/java/gbuy/Product.java
+++ src/main/java/gbuy/Product.java
@@ -1,20 +1,27 @@
 package gbuy;
 
 public class Product {
+
+    //  INSTANCE VARIABLES
     private String productName;
     private double productPrice;
 
+    //  CONSTRUCTOR
     public Product(String name, double price){
         this.productName = name;
         this.productPrice = price;
     }
 
+    //  GETTERS
     public String getProductName() {
         return productName;
     }
-
     public double getProductPrice() {
         return productPrice;
     }
+
+    //  SETTERS
+    public void setProductName(String productName) { this.productName = productName; }
+    public void setProductPrice(double productPrice) { this.productPrice = productPrice; }
 }
 
